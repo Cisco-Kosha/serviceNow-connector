98c7749a8eca134c578d3c1dab0c07f3f88c4da6
Pavan Sudheendra <pavan0591@gmail.com>
2023-05-19 15:40:37 +0100

    remove custom code

--- pkg/models/shared.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Specification struct {
-	Username   string `json:"username,omitempty"`
-	Password   string `json:"password,omitempty"`
-	DomainName string `json:"domain_name,omitempty"`
-}
